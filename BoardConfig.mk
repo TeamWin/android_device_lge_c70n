05c9ae24dec7b092e0dec8a6ace0646fdca7eea1
Jakov Glavina <cyber.shot.jakov@gmail.com>
2017-02-27 07:25:55 -0800

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,73 @@
 
 DEVICE_PATH := device/lge/c70n
 
-include device/lge/msm8916-common/BoardConfigCommon.mk
+# Platform
+TARGET_BOARD_PLATFORM := msm8916
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
+TARGET_NO_BOOTLOADER := true
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=c70n user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=508m
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := c70n
+
+# Audio
+AUDIO_FEATURE_ENABLED_FM := true
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+AUDIO_FEATURE_ENABLED_NEW_SAMPLE_RATE := true
+AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
+BOARD_USES_ALSA_AUDIO := true
+TARGET_USES_QCOM_MM_AUDIO := true
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# CMHW
+BOARD_HARDWARE_CLASS := $(DEVICE_PATH)/cmhw/
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Display
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+TARGET_HAVE_NEW_GRALLOC := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_ION := true
+USE_OPENGL_RENDERER := true
+
+# FM
+TARGET_QCOM_NO_FM_FIRMWARE := true
+
+# GPS
+TARGET_GPS_HAL_PATH := $(DEVICE_PATH)/gps
+TARGET_PROVIDES_GPS_LOC_API := true
+
+# Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_PATH)/init/init_c70n.c
 
 TARGET_KERNEL_CONFIG := c70n-mini_defconfig
 
@@ -26,6 +92,65 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 25165824      # 16384 * 1024 mmcblk0p31
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 25165824  # 16484 * 1024 mmcblk0p32
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2576980992  # 2375680 * 1024 mmcblk0p41
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 3833488384 # 4751232 * 1024 mmcblk0p42
+
+# Power
+TARGET_POWERHAL_VARIANT := qcom
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# SELinux
+
+include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += \
+    $(DEVICE_PATH)/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    akmd09912.te \
+    atfwd.te \
+    atvc.te \
+    batt_health.te \
+    bluetooth_loader.te \
+    device.te \
+    file.te \
+    file_contexts \
+    healthd.te \
+    init.te \
+    init_shell.te \
+    mm-qcamerad.te \
+    property.te \
+    property_contexts \
+    qseecomd.te \
+    rild.te \
+    rmt_storage.te \
+    stml0xx.te \
+    surfaceflinger.te \
+    system_server.te \
+    tee.te \
+    ueventd.te \
+    vold.te \
+    wcnss_service.te
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # TWRP
 TW_THEME := portrait_hdpi
