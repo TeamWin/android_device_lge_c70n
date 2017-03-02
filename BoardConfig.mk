b0d90f277e242effef13aa403bf2a64cae273eb0
Jakov Glavina <cyber.shot.jakov@gmail.com>
2017-03-02 15:29:13 +0100

    remove dirty fix for booting into recovery with hardware buttons

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,4 +63,4 @@ TARGET_PREBUILT_KERNEL := device/lge/c70n/recovery/kernel
 TW_THEME := portrait_hdpi
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_IGNORE_MISC_WIPE_DATA := true
+
